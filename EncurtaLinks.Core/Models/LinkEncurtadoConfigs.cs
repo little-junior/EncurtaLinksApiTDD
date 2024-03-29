ccdb71a65f1017bcfdfb702c9cb92b43a4d4a767
little-junior <rampagedolol@outlook.com>
2024-03-28 22:59:51 -0300

    feat: add caracteresPossiveis dict to configs

--- EncurtaLinks.Core/Models/LinkEncurtadoConfigs.cs
+++ EncurtaLinks.Core/Models/LinkEncurtadoConfigs.cs
@@ -13,5 +13,11 @@ namespace EncurtaLinks.Core.Models
         public const int MaxCaracteres = 7;
         public const int SegundosValido = 300;
         public const string UrlPadrao = "http://localhost:5229/";
+        public static readonly Dictionary<int, string> caracteresPossiveis = new()
+        {
+            {1, "0123456789"},
+            {2, "abcdefghijklmnopqrstuvwxyz"},
+            {3, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
+        };
     }
 }
